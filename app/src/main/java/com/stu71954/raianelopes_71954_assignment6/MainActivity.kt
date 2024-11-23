16b9fc42caccc685df2b2826394fc47c1d7214a4
Raianelc <71954@student.dorset-college.ie>
2024-11-23 17:13:41 +0000

    changes

--- app/src/main/java/com/stu71954/raianelopes_71954_assignment6/MainActivity.kt
+++ app/src/main/java/com/stu71954/raianelopes_71954_assignment6/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
-import com.stu71954.raianelopes_71954_assignment6.home.HomeScreen
-import com.stu71954.raianelopes_71954_assignment6.loginUser.LoginScreen
+import com.stu71954.raianelopes_71954_assignment6.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +14,7 @@ class MainActivity : ComponentActivity() {
         FirebaseApp.initializeApp(this)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            Surface(color = MaterialTheme.colorScheme.background) {
-                NavHost(navController = navController, startDestination = "login") {
-                    composable("login") { LoginScreen(navController) }
-                    composable("home") { MapScreen() }
-                }
-            }
+            AppNavigation()
         }
     }
 }
-
--- app/src/main/java/com/stu71954/raianelopes_71954_assignment6/navigation/Navigation.kt
+++ app/src/main/java/com/stu71954/raianelopes_71954_assignment6/navigation/Navigation.kt
@@ -1,3 +1,21 @@
 package com.stu71954.raianelopes_71954_assignment6.navigation
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.stu71954.raianelopes_71954_assignment6.home.HomeScreen
+import com.stu71954.raianelopes_71954_assignment6.loginUser.LoginScreen
 
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colorScheme.background) {
+        NavHost(navController = navController, startDestination = "login") {
+            composable("login") { LoginScreen(navController) }
+            composable("home") { HomeScreen() }
+        }
+    }
+}
